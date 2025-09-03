5a51879d0b8e0e0f9a24e9f4538efc44f5d8b95e
Subhajit majhi <subhajitmajhi@Subhajits-MacBook-Air.local>
2025-09-03 22:09:00 +0530

    Added vercel json file

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,7 +5,7 @@ import CommercialClients from '../components/CommercialClients'
 export default function Projects() {
   return (
     <>
-      <section className="max-w-5xl mx-auto px-4 pt-12 pb-4">
+      <section className="max-w-5xl mx-auto px-4 pt-32 pb-4">
         <h1 className="text-4xl font-bold">Projects</h1>
       </section>
       <SpacesWeShaped />
